539d679041ea6ce733d4f59c80517ecc4ee7730e
retroruiner <jostonfer@gmail.com>
2025-04-23 02:07:41 +0200

    added github workflow

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -5,6 +5,6 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "../..", ".env"))
 
-DATABASE_URI = os.getenv("DATABASE_URI")
-engine = create_engine(DATABASE_URI)
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
